4ec4773ed1dec1b4448ab73ff29eca740a83fbe3
Kristen Coy <kristenbwinslow@gmail.com>
2022-01-29 20:05:25 -0500

    Added board and game components, updated styles

--- src/index.js
+++ src/index.js
@@ -1,17 +1,110 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { useState } from "react";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Board(props) {
+  const renderSquare = function (i) {
+    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
+  };
+
+  console.log(props.squares);
+
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+function Game() {
+  const [state, setState] = useState({
+    squares: Array(9).fill(null),
+    xIsNext: true,
+  });
+
+  const handleClick = (i) => {
+    const _squares = state.squares.slice();
+    if (calculateWinner(_squares) || _squares[i]) {
+      return;
+    }
+    _squares[i] = state.xIsNext ? "X" : "O";
+    setState({ squares: _squares, xIsNext: !state.xIsNext });
+  };
+
+  const winner = calculateWinner(state.squares);
+  let status;
+  if (winner) {
+    status = winner + " wins!";
+  } else {
+    status = "It's " + (state.xIsNext ? "X" : "O") + "'s turn.";
+  }
+
+  const resetBoard = () => {
+    setState({
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    });
+  };
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={state.squares} onClick={(i) => handleClick(i)} />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{/* TODO */}</ol>
+        <button className="button" onClick={resetBoard}>
+          Restart
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
